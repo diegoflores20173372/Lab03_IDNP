6bb53e48106cda16c8a247a8bd29be6bc0cb00ea
diegoflores20173372 <40549405+diegoflores20173372@users.noreply.github.com>
2021-10-03 06:42:26 -0500

    Testeo Inicial y Main Activity

--- app/src/main/java/com/lab03/lab03_idnp/MainActivity.kt
+++ app/src/main/java/com/lab03/lab03_idnp/MainActivity.kt
@@ -4,7 +4,10 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.AlarmClock.EXTRA_MESSAGE
+import android.service.autofill.TextValueSanitizer
 import android.view.View
+import android.widget.TextView
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,6 +17,39 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val txtDNI = findViewById<TextView>(R.id.txtDNI).apply {
+            text = patientData[0]
+        }
+
+        val txtFullName = findViewById<TextView>(R.id.txtFullName).apply {
+            text = patientData[1]
+        }
+
+        val txtDirection = findViewById<TextView>(R.id.txtDirection).apply {
+            text = patientData[2]
+        }
+
+        val txtEmail = findViewById<TextView>(R.id.txtEmail).apply {
+            text = patientData[3]
+        }
+
+        val txtWeight = findViewById<TextView>(R.id.txtWeight).apply {
+            text = patientVisit[0]
+        }
+
+        val txtTemperature = findViewById<TextView>(R.id.txtTemperature).apply {
+            text = patientVisit[1]
+        }
+
+        val txtPressure = findViewById<TextView>(R.id.txtPressure).apply {
+            text = patientVisit[2]
+        }
+
+        val txtSaturation = findViewById<TextView>(R.id.txtSaturation).apply {
+            text = patientVisit[3]
+        }
+
     }
 
     /** We call the activity for the button to the patient */
