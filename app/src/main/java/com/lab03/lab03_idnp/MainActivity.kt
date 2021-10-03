69481627186de433b425826d65c3c09bec4b01aa
diegoflores20173372 <40549405+diegoflores20173372@users.noreply.github.com>
2021-10-03 05:43:43 -0500

    Correo y Envio de datos parcial

--- app/src/main/java/com/lab03/lab03_idnp/MainActivity.kt
+++ app/src/main/java/com/lab03/lab03_idnp/MainActivity.kt
@@ -3,11 +3,12 @@ package com.lab03.lab03_idnp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.AlarmClock.EXTRA_MESSAGE
 import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
-    val patientData = arrayOf("73641789", "Diego Flores Camargo", "Calle Tarapaca 208 Miraflores")
+    val patientData = arrayOf("73641789", "Diego Flores Camargo", "Calle Tarapaca 208 Miraflores", "dflorescam@unsa.edu.pe")
     val patientVisit = arrayOf("70kg", "73C", "110mmHg", "95%")
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,8 +26,9 @@ class MainActivity : AppCompatActivity() {
 
     /** We call the activity for the button to the visit of the patient */
     fun visitPatientButton(view: View){
+        val dni = patientData.get(0)
         val intent = Intent(this, VisitActivity::class.java).apply {
-
+            putExtra(EXTRA_MESSAGE, dni)
         }
         startActivity(intent)
     }
--- app/src/main/java/com/lab03/lab03_idnp/VisitActivity.kt
+++ app/src/main/java/com/lab03/lab03_idnp/VisitActivity.kt
@@ -2,10 +2,17 @@ package com.lab03.lab03_idnp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.AlarmClock.EXTRA_MESSAGE
 
 class VisitActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_visit)
+
+        /** Capturing the dni from the MainActivity */
+        val dni = intent.getStringExtra(EXTRA_MESSAGE)
+
+
+
     }
 }
