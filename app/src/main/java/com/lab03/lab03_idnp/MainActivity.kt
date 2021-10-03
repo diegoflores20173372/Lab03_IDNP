1bc7e611018417675ba768b1fc956788a893d764
diegoflores20173372 <40549405+diegoflores20173372@users.noreply.github.com>
2021-10-03 05:21:34 -0500

    launch activities

--- app/src/main/java/com/lab03/lab03_idnp/MainActivity.kt
+++ app/src/main/java/com/lab03/lab03_idnp/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.lab03.lab03_idnp
 
+import Patient
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
+
+    val patient = Patient(73641789, "Diego Flores Camargo", "Calle Tarapacá 208 Miraflores")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    /** We call the activity for the button to the patient */
+    fun patientViewButton(view: View){
+        val intent = Intent(this, PatientActivity::class.java).apply {
+
+        }
+        startActivity(intent)
+    }
+
+    /** We call the activity for the button to the visit of the patient */
+    fun visitPatientButton(view: View){
+        val intent = Intent(this, VisitActivity::class.java).apply {
+
+        }
+        startActivity(intent)
+    }
+
 }
